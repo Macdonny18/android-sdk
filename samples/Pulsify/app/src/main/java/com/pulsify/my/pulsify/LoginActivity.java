0b8c6b04072eac6c27629f1748b89ba75699b527
Macdonny Saintil <macdonnysaintil@hotmail.com>
2016-10-23 01:18:28 -0400

    Pulsify frontend

---
+++ samples/Pulsify/app/src/main/java/com/pulsify/my/pulsify/LoginActivity.java
@@ -0,0 +1,34 @@
+package com.pulsify.my.pulsify;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        Thread timerThread = new Thread(){
+            public void run(){
+                try{
+                    sleep(3000);
+                }catch(InterruptedException e){
+                    e.printStackTrace();
+                }finally{
+                    Intent intent = new Intent(LoginActivity.this,MainActivity.class);
+                    startActivity(intent);
+                }
+            }
+        };
+        timerThread.start();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        finish();
+    }
+}
